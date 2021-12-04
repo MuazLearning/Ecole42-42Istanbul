e8bd61b77d86b713fa3eb1eebe9d75cef05eb298
Muaz MEMİŞ <muazmemis@gmail.com>
2021-12-04 17:47:25 +0300

    C_01 finish

---
+++ C_01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmemis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 17:32:52 by mmemis            #+#    #+#             */
+/*   Updated: 2021/12/04 17:41:32 by mmemis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	read_index;
+	int	index;
+	int	swapped;
+
+	read_index = 0;
+	while (true)
+	{
+		swapped = 0;
+		index = 0;
+		while (index < size)
+		{
+			if (tab[index] > tab[index + 1])
+			{
+				ft_swap(&tab[index], &tab[index + 1]);
+				swapped = 1;
+			}
+			index++;
+		}
+		if (!swapped)
+			break ;
+		read_index++;
+	}
+}
