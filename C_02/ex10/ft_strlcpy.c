367b57246b2978dad308171f36c7df95eb85be05
Muaz MEMİŞ <muazmemis@gmail.com>
2021-12-11 20:29:29 +0300

    Edit C_02

--- C_02/ex10/ft_strlcpy.c
+++ C_02/ex10/ft_strlcpy.c
@@ -32,19 +32,19 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	}
 	return (count);
 }
-#include <stdio.h>
-int        main(void)
-{
-    char src[] = "Source";
-    char dest[] = "";
-    unsigned int n;
-
-    n = 4;
-
-    printf("BEFORE\n\tsrc: %s\n\tdes: %s\n\tsize: %d\n", src, dest, n);
-
-    n = ft_strlcpy(dest, src, n);
-
-    printf("AFTER\n\tsrc: %s\n\tdes: %s\n\tsize: %d", src, dest, n);
-    return (0);
-}
+//#include <stdio.h>
+//int        main(void)
+//{
+//    char src[] = "Source";
+//    char dest[] = "";
+//    unsigned int n;
+//
+//    n = 4;
+//
+//    printf("BEFORE\n\tsrc: %s\n\tdes: %s\n\tsize: %d\n", src, dest, n);
+//
+//    n = ft_strlcpy(dest, src, n);
+//
+//    printf("AFTER\n\tsrc: %s\n\tdes: %s\n\tsize: %d", src, dest, n);
+//    return (0);
+//}
