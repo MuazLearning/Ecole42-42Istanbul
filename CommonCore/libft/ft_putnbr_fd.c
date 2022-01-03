bd293c6b25ecabcba4cc868767b4fb8da88fc88a
Muaz MEMİŞ <muazmemis@gmail.com>
2022-01-03 19:21:59 +0300

    Common Core Day 1

---
+++ CommonCore/libft/ft_putnbr_fd.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmemis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/03 19:07:13 by mmemis            #+#    #+#             */
+/*   Updated: 2022/01/03 19:19:29 by mmemis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(214748364, fd);
+		ft_putchar_fd('8', fd);
+	}
+	else if (n >= 0 && n < 10)
+	{
+		ft_putchar_fd((n + '0'), fd);
+	}
+	else if (n >= 10)
+	{
+		ft_putnbr_fd((n / 10), fd);
+		ft_putchar_fd((n % 10 + '0'), fd);
+	}
+	else
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd((n * -1), fd);
+	}
+}
